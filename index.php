809117d5d431ba996a5c710339ff7f61df8599c7
ilmiilhafa <ilmi.ilhafa@gmail.com>
2016-04-28 16:21:03 +0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title> SISTEM INFORMASI KEPEGAWAIAN </title>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+	<body>
+		<div class="header">		
+	<div class="gambar">
+			<h1>Dinas Sosial, Tenaga Kerja & Transmigrasi</h1>
+			<h2>PAMEKASAN</h2>
+		</div>
+		
+	
+		</div>
+		<div class="menu">
+			<ul>
+				<li><a href="#">Home</a></li>
+				
+				<li><a href="http://localhost/SIPEG/profil.php">Profil</a></li>
+				
+				<li><a href="http://localhost/SIPEG/admin/login.php">Login</a>
+				
+				</li>
+			</ul>
+		</div>
+		<div class="konten">
+			
+<div id="sidebar">
+<h1><strong><marquee>Sistem Informasi Kepegawaian </marquee></strong></h1>
+<h2>Deskripsi</h2>
+<p>Dinas Sosial Tenaga Kerja dan Transmigrasi merupakan urusan pelaksana otonom daerah di bidang sosial tenaga kerja dan transmigrasi.Dinas Sosial Tenaga Kerja dan Transmigrasi, dipimpin oleh seorang Kepala Dinas yang berada di bawah dan bertanggungjawab kepada Bupati melalui Sekretaris Daerah.</p>
+<p>Dinas Sosial Tenaga Kerja dan Transmigrasi mempunyai tugas melaksanakan urusan pemerintah daerah di bidang sosial tenaga kerja dan transmigrasi berdasarkan azaz otonomi dan tugas pembantuan.</p>
+<p>Alamat Dinas: Jln Raya Panglegur POBOX.14 </p>
+<p>Telepon : (0324)322666</p>
+<p>Kode Pos : 69371</p>
+<p>Jumlah Pegawai :61</p>
+</div>
+			
+</div>
+<div class="footer">
+<div class="logo">
+	<h1>Copyright©2014.ilmi_ifa.com </h1>
+</div>
+</div>
+</body>
+</html>
---
+++ profil.php
@@ -0,0 +1,63 @@
+<html>
+	<head>
+		<title> SISTEM INFORMASI KEPEGAWAIAN </title>
+		<link rel="stylesheet" type="text/css" href="profil.css"/>
+	</head>
+	<body>
+		<div class="header">		
+	<div class="gambar">
+			<h1>Dinas Sosial, Tenaga Kerja & Transmigrasi</h1>
+			<h2>PAMEKASAN</h2>
+		</div>
+		
+	
+		</div>
+		<div class="menu">
+			<ul>
+				<li><a href="http://localhost/SIPEG/index.php">Home</a></li>
+				
+				<li><a href="http://localhost/SIPEG/profil.php">Profil</a></li>
+				
+				<li><a href="http://localhost/SIPEG/admin/login.php">Login</a>
+				
+				</li>
+			</ul>
+		</div>
+		<div class="konten">
+			
+<div id="sidebar">
+<h1><strong><marquee>Sistem Informasi Kepegawaian </marquee></strong></h1>
+<h2>PROFIL</h2>
+<p>Nama Dinas: Dinas Sosial Tenaga Kerja dan Transmigrasi </p>
+<p>Alamat Dinas: Jln Raya Panglegur POBOX.14 </p>
+<p>Telepon : (0324)322666</p>
+<p>Kode Pos : 69371</p>
+<p>Jumlah Pegawai :61</p>
+<h2>VISI</h2>
+<p>Sejalan dengan Kabupaten Pamekasan serta sesuai dengan Tugas Pokok dan Fungsi Dinas Sosial, Tenaga kerja dan Transmigrasi Kabupaten Pamekasan maka Visi Dinas Sosial, Tenaga Kerja dan Transmigrasi Kabupaten Pamekasan sementara mengacu pada penggabungan dari visi kantor sosial dan visi dari Dinas Tenaga Kerja dan Transmigrasi Pamekasan sebagai berikut :</p>
+<p>Makna visi tersebut adalah :</p>
+<p>- Terwujudnya peningkatan taraf kesejahteraan sosial, penyandang masalah Kesos melalui usaha bersama pemerintah dan masyarakat.</p>
+<p>- Terwujudnya pelayanan, perlindungan, pemenuhan kualitas Tenaga Kerja dan Transmigrasi.</p>
+<p>Makna visi dari penggabungan tersebut adalah :</p>
+<p>- Terwujudnya peningkatan taraf kesejahteraan sosial, penyandang masalah Kesos melalui usaha bersama pemerintah dan masyarakat.</p>
+<p>- Terwujudnya pelayanan adalah keadaan dimana kebutuhan tenaga kerja / pencari kerja dan transmigran dapat difasilitasi sesuai dengan peraturan perundang-undangan yang berlaku dengan cepat, tepat dan mudah. </p>
+<p>- Terwujudnya perlindungan adalah keadaan dimana tenaga kerja yang telah bekerja baik di dalam dan di luar Negri merasa aman dari segala bentuk perlakuan yang tidak manusiawi atau melanggar hak-hak Asasi Manusia.</p>
+<p>- Terwujudnya pemenuhan kualitas dan kuantitas, adalah keadaan dimana tenaga kerja dan transmigran baik pengetahuan dan keterampilannya memenuhi standar yang ditentukan serta jumlah yang dibutuhkan terpenuhi.</p>
+<h2>MISI</h2>
+<p>Untuk mewujudkan Visi yang ditetapkan maka Dinas Sosial, Tenaga Kerja dan Transmigrasi Kabupaten Pamekasan mempunyai Misi yang jelas yaitu sementara mengacu pada penggabungan Misi dari kantor Sosial, dan Misi Dinas Tenaga Kerja dan Transmigrasi Kabupaten Pamekasan yaitu :</p>
+<p>a.	Mendukung perluasan dan peningkatan pelaksanaan pembangunan Kesos oleh pemerintah dan masyarakat guna meningkatkan taraf kesejahteraan sosial, penyandang maslah sosial </p>
+<p>b.	Mendorong terciptanya Penempatan, Perluasan dan Pelatihan Kerja.</p>
+<p>c.	Mendorong terciptanya hubungan Industrial yang harmonis dan dinamis.</p>
+<p>d.	Meningkatkan perlindungan Tenaga Kerja Keselamatan dan Kesehatan Kerja.</p>
+<p>e.	Mendorong terciptanya mobilitas penduduk yang harmonis dan dinamis.</p>
+<p>f.	Pembinaan kepada lembaga Sosial Organisi Kemasyarakatan yang berkaitan dengan program ketransmigrasian.</p>
+</div>
+			
+</div>
+<div class="footer">
+<div class="logo">
+	<h1>Copyright©2014.ilmi_ifa.com </h1>
+</div>
+</div>
+</body>
+</html>
